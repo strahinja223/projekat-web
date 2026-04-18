c1c0fc8049950c479a5d08caebd505f37f2c6276
strahinja223 <strahinjabukvic49@gmail.com>
2026-04-18 16:16:31 +0200

    Enum uloga

--- src/main/java/com/example/projekat/ProjekatApplication.java
+++ src/main/java/com/example/projekat/ProjekatApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjekatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjekatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjekatApplication.class, args);
+    }
 
 }
